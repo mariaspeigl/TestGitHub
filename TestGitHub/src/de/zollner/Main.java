b20a0f3049deddd453ddd8c1db5654e668489da5
q32742 <q32742@NZZ064662W7.intern.zollner.de>
2016-09-20 08:17:12 +0200

    My first commit

---
+++ TestGitHub/src/de/zollner/Main.java
@@ -0,0 +1,20 @@
+package de.zollner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		SalesData data = new SalesData();
+		
+		displayGreeting();
+		data.display();
+
+	}
+
+	private static void displayGreeting() {
+		System.out.println("HELLO HAPPY SALES PEOPLE!");
+		System.out.println("THIS APP SHOWS SALES DATA");
+		System.out.println("Test1");
+		
+	}
+
+}
---
+++ TestGitHub/src/de/zollner/SalesData.java
@@ -0,0 +1,12 @@
+package de.zollner;
+
+public class SalesData {
+
+	int data[] = {0, 4, 42};
+	
+	public void display() {
+		System.out.println("TODO: Implement");
+		
+	}
+
+}
